1719f3cb1a5a20c3f2a19c9854cac82ab7e5e2f7
Shuang.W <66452432+swnotmetal@users.noreply.github.com>
2024-11-05 21:48:31 +0200

    fixed the structure2

---
+++ .eslintrc.js
@@ -0,0 +1,24 @@
+// server/.eslintrc.js
+module.exports = {
+    env: {
+      node: true,
+      commonjs: true,
+      es2021: true,
+    },
+    extends: ['eslint:recommended'],
+    parserOptions: {
+      ecmaVersion: 'latest',
+    },
+    plugins: ['cypress'],
+    rules: {
+      indent: ['error', 2],
+      'linebreak-style': ['error', 'windows'],
+      quotes: ['error', 'single'],
+      semi: ['error', 'never'],
+      eqeqeq: 'error',
+      'no-trailing-spaces': 'error',
+      'object-curly-spacing': ['error', 'always'],
+      'arrow-spacing': ['error', { before: true, after: true }],
+    },
+    ignores: ['**/node_modules/**', '**/*.spec.js', '**/build/**, **/dist/**'],
+  };
---
+++ client/.eslintrc.js
@@ -0,0 +1,31 @@
+// server/client/.eslintrc.js
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:react-hooks/recommended',
+  ],
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: ['react', 'react-hooks'],
+  rules: {
+    indent: ['error', 2],
+    'linebreak-style': ['error', 'windows'],
+    quotes: ['error', 'single'],
+    semi: ['error', 'never'],
+    'react/react-in-jsx-scope': 'off',
+    'react/prop-types': 'off',
+  },
+  ignores: ['**/build/**', '**/dist/**', '**/*.spec.js'],
+};
