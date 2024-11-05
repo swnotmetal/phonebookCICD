254daa49749635849104e8e42595f81bbd41989e
Shuang.W <66452432+swnotmetal@users.noreply.github.com>
2024-11-05 20:38:06 +0200

    resetting .git in this dir now

---
+++ client/src/components/InputFiled.jsx
@@ -0,0 +1,14 @@
+const InputField = ({ text, label, value, handleInputChange }) => {
+
+	const handleChange = (event) => {
+		console.log('InputField:', event.target.value)
+		handleInputChange(event)
+	}
+	return (
+		<div>
+			{label} <input value={value} onChange={handleChange} placeholder={text} />
+		</div>
+	)
+}
+
+export default InputField
