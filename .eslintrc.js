7cd92834cfe65830127df5312ee0cb8307fae227
Shuang.W <66452432+swnotmetal@users.noreply.github.com>
2024-11-07 05:51:35 +0200

    combine eslintrc

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,23 +1,49 @@
-// server/.eslintrc.js
 module.exports = {
-    env: {
-      node: true,
-      commonjs: true,
-      es2021: true,
+  env: {
+    es2021: true,
+    'cypress/globals': true,
+    node: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:react-hooks/recommended',
+    'plugin:cypress/recommended'
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+  },
+  plugins: ['cypress'],
+  rules: {
+    indent: ['error', 2],
+    'linebreak-style': ['error', 'windows'],
+    quotes: ['error', 'single'],
+    semi: ['error', 'never'],
+    eqeqeq: 'error',
+    'no-trailing-spaces': 'error',
+    'object-curly-spacing': ['error', 'always'],
+    'arrow-spacing': ['error', { before: true, after: true }],
+    'react/react-in-jsx-scope': 'off', // If using React 17+
+    'react/prop-types': 'off'
+  },
+  overrides: [
+    {
+      files: ['client/**/*.js', 'client/**/*.jsx'],
+      env: {
+        browser: true,
+      },
+      settings: {
+        react: {
+          version: 'detect',
+        },
+      },
     },
-    extends: ['eslint:recommended'],
-    parserOptions: {
-      ecmaVersion: 'latest',
-    },
-    plugins: ['cypress'],
-    rules: {
-      indent: ['error', 2],
-      'linebreak-style': ['error', 'windows'],
-      quotes: ['error', 'single'],
-      semi: ['error', 'never'],
-      eqeqeq: 'error',
-      'no-trailing-spaces': 'error',
-      'object-curly-spacing': ['error', 'always'],
-      'arrow-spacing': ['error', { before: true, after: true }],
-    },
-  };
+    {
+      files: ['server/**/*.js'],
+      env: {
+        node: true,
+        commonjs: true,
+      },
+    }
+  ]
+}
